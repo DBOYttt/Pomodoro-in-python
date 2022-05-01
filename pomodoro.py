e1d45f1facbc2e163737c3d6bb794bdacf1ac5cc
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-01 10:27:38 +0200

    (update) saving data

--- pomodoro.py
+++ pomodoro.py
@@ -1,24 +1,29 @@
 import datetime
+from gettext import find
 import os
 
 while 1:
     x = datetime.datetime.now()
 
     y = int(x.strftime("%M"))
+    d = open('pomodoro1.txt', 'a')
+    d.write(str(y))
+    d.close()
+    f = open('pomodoro1.txt', 'r')
+    f.read()
+    find(y in d)
+    if y == y
 
-f = open("pomodoro.txt", "a")
-f.write(str(y))
-f.close()
 
-f = open('pomodoro.txt', 'r')
+d = open('pomodoro1', 'r')
+z = d.read()
+if t in z :
+    print('here')
 
-z = f.read()
 
-print('start point: ' + z)
-
-if y == int(z) + 1:
-    print('its time to rest :)')
-    if True:
-        os.remove("pomodoro.txt")
+#if y == int(z) + 1:
+ #   print('its time to rest :)')
+  #  if True:
+   #     os.remove("pomodoro.txt")
         
 
